a3cc2f87be6372a7ecb0f250050894d0fe1bb3e7
LiXing <lixing@shein.com>
2024-02-18 22:08:11 +0800

    feat: id

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'gh_bb062638ff83',
+      id: 'oKMJ26iiuewWCckOIixCYt5_LD8E',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'xye8wNRL7eOMyKz4tCM2_1jDuf_Jttd2i9zbTAQHrK8',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
